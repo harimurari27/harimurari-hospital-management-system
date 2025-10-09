649f8ad87b9089ebec1f4c00bd4e9e5d5bcba461
Naveen <katrapallynaveenkumar1@gmail.com>
2025-10-09 18:51:10 +0530

    second

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,6 +36,10 @@ const Navbar = () => {
           <li className='py-1'>CONTACT</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
+        <li className='py-1 px-3 text-black-500 cursor-pointer border border-black rounded'
+          onClick={() => window.open('https://doctor-appointment-admin-v9b3.onrender.com', '_blank')}>
+          ADMIN PANEL
+        </li>
       </ul>
 
       <div className='flex items-center gap-4 '>
@@ -67,6 +71,15 @@ const Navbar = () => {
             <NavLink onClick={() => setShowMenu(false)} to='/doctors' ><p className='px-4 py-2 rounded full inline-block'>ALL DOCTORS</p></NavLink>
             <NavLink onClick={() => setShowMenu(false)} to='/about' ><p className='px-4 py-2 rounded full inline-block'>ABOUT</p></NavLink>
             <NavLink onClick={() => setShowMenu(false)} to='/contact' ><p className='px-4 py-2 rounded full inline-block'>CONTACT</p></NavLink>
+            <p
+              onClick={() => {
+                setShowMenu(false)
+                window.open('https://doctor-appointment-admin-v9b3.onrender.com', '_blank')
+              }}
+              className='px-4 py-2  full inline-block text-black-500 cursor-pointer border border-black rounded'
+            >
+              ADMIN PANEL
+            </p>
           </ul>
         </div>
       </div>
